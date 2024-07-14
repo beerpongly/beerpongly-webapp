0efa0a1a4864ab5d58ea3fe73b32698be84c0c10
Lewis Cabban <beerpongly@gmail.com>
2024-07-14 19:33:57 +0930

    updating link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2024 <a href="/" className="hover:underline">Beerpongly</a></span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2024 <Link href="/" className="hover:underline">Beerpongly</Link></span>
     </div>
       </Container>
       </div>
